c0f25fcb0bdc7d40f49eac1938aa976e5480ad01
Giacomo Cirò <130163233+BrightManx@users.noreply.github.com>
2023-09-10 21:05:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from backend import show_explore, show_try
+# from backend import show_explore, show_try
 
 st.set_page_config(
     page_title="House Price Predictor",
@@ -11,9 +11,9 @@ st.set_page_config(
 pages = ['Try the model', 'Explore the model']
 page = st.sidebar.selectbox('', pages)
 
-# Pages
-if page == 'Try the model':
-    show_try()
+# # Pages
+# if page == 'Try the model':
+#     show_try()
 
-if page == 'Explore the model':
-    show_explore()
+# if page == 'Explore the model':
+#     show_explore()
