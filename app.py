c3bc4babb0f1801af6d8fbb01a7d9a80f2685e39
Giacomo Cirò <130163233+BrightManx@users.noreply.github.com>
2023-09-10 21:03:44 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from backend import show_explore, show_try
+
+st.set_page_config(
+    page_title="House Price Predictor",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# Sidebar 
+pages = ['Try the model', 'Explore the model']
+page = st.sidebar.selectbox('', pages)
+
+# Pages
+if page == 'Try the model':
+    show_try()
+
+if page == 'Explore the model':
+    show_explore()
